960cd30a54f1eeeeb54e851f37d1fca8cb7eeff0
이승환 <seaung13@gmail.com>
2025-06-10 01:22:48 +0900

    refactor: lesson 5-15 BDD 스타일로 작성하기 (총 주문 금액 계산 테스트를 BDD 스타일로 리팩토링)

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -105,8 +105,10 @@ class CafeKioskTest {
                 .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
     }
 
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -114,6 +116,10 @@ class CafeKioskTest {
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
-        assertThat(cafeKiosk.calculateTotalPrice()).isEqualTo(8500);
+        // when
+        int totalPrice = cafeKiosk.calculateTotalPrice();
+
+        // then
+        assertThat(totalPrice).isEqualTo(8500);
     }
 }
