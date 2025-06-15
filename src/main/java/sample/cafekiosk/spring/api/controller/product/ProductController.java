058f8bfe99ad6ece4769b169b1f2ddd43df93259
이승환 <seaung13@gmail.com>
2025-06-16 05:55:08 +0900

    feat: lesson 6-24 Presentation Layer 테스트 (1) - 관리자 페이지 신규 상품 등록 기능 추가 (TDD 기반 구현)

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -2,7 +2,10 @@ package sample.cafekiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.controller.product.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 
@@ -17,5 +20,10 @@ public class ProductController {
     @GetMapping("/api/v1/products/selling")
     public List<ProductResponse> getSellingProducts() {
         return productService.getSellingProducts();
+    }
+
+    @PostMapping("api/v1/products/new")
+    public void createProduct(@RequestBody ProductCreateRequest request) {
+        productService.createProduct(request);
     }
 }
